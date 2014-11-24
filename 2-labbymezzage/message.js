75cc8b3178c888b43007a93892be37355d54bfdb
Martin Carlsson <mc22dg@student.lnu.se>
2014-11-24 10:43:58 +0000

    lagt till alla filer som behövs

---
+++ 2-labbymezzage/message.js
@@ -0,0 +1,47 @@
+"use strict";
+
+window.onload = function(){
+    
+    function message(message, date){
+        
+    	this.getText = function(){
+                return message;
+        }
+        this.setText = function(_text){
+                message = _text;
+        }
+        this.getDate = function(){
+                 return date;
+        }
+        this.setDate = function(_date){
+                   date = _date;
+        }
+        }
+Message.prototype.toString = function(){
+        return this.getText()+" ("+this.getDate()+")";
+        }
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
