a8a5c70fa0d4d2f7002f42815fef1db63a7f7b41
Martin Carlsson <mc22dg@student.lnu.se>
2014-12-02 19:38:19 +0000

    lagt till kommentarer i up 2

--- 2-labbymezzage/messageBoard.js
+++ 2-labbymezzage/messageBoard.js
@@ -4,7 +4,8 @@ var MessageBoard = {
    
     messages:[],
     
-    init:function()//Denna funktion styr så att när man klickar på skicka knappen alt. trycker Enter, skickas meddelandet iväg till  
+    init:function()//Denna funktion styr så att när man klickar på "skicka" knappen alt. trycker "Enter", skickas meddelandet iväg. 
+    //Trycker man "enter" + "shift" så får man en radbrytning. Meddelanden går heller inte att skicka om kommentarsfältet är tomt.
     {
         var send = document.getElementById("btn"); 
         send.onclick = function(e){
@@ -34,18 +35,19 @@ var MessageBoard = {
     sendMessage:function()
     {
        var text = document.getElementById("comment").value; //Lägger in värdet i comment och lägger det i variabeln text.
-       var message = new Message(text, new Date()); //
+       var message = new Message(text, new Date()); //Här sätts datumet på meddelandet
        MessageBoard.messages.push(message);
        var arrayslot = MessageBoard.messages.length-1;
        console.log(arrayslot);
-       document.getElementById("numberOfMess").innerHTML = "Antal meddelanden: "+MessageBoard.messages.length;
+       document.getElementById("numberOfMess").innerHTML = "Antal meddelanden: "+MessageBoard.messages.length; //Här räknas antalet
+       //meddelanden som gjorts i chatten. Dessa mäts med hjälp utav att mäta arrayens längd.
        document.getElementById("comment").value = "";
        MessageBoard.RenderMessage(arrayslot);
        
 
     },
     
-    RenderMessages:function()
+    RenderMessages:function() //Denna funktion samarbetar med RenderMessage och skriver ut varje meddelande genom arrayen
     {
         document.getElementById("message").innerHTML = "";
         for (var i = 0; i < MessageBoard.messages.length; i++) {
@@ -54,7 +56,7 @@ var MessageBoard = {
         
     },
     
-    RenderMessage:function(arrayslot)
+    RenderMessage:function(arrayslot) //Har ansvaret att skriva ut ett meddelande
     {
         console.log(MessageBoard.messages);
        var div = document.createElement("div");
@@ -62,16 +64,16 @@ var MessageBoard = {
        var textfalt = document.createElement("p");
        textfalt.innerHTML = MessageBoard.messages[arrayslot].getHTMLText();
        
-
+    
        var messagesList = document.getElementById("message");
-       
+        //Här skrivs ikonerna och tiden ut   
        var imgClock = document.createElement("img") 
        var imgDelete = document.createElement("img")
        imgClock.src = "clock.png";
        imgDelete.src = "trash.png";
        var timestamp = document.createElement("div");
        timestamp.innerHTML = MessageBoard.messages[arrayslot].getDateText();
-        imgDelete.onclick = function(){
+        imgDelete.onclick = function(){     //Här är funktionen/if-satsen för att radera meddelanden
            if(confirm("Är du säker på att du vill radera meddelandet"))
            {
     
@@ -92,7 +94,7 @@ var MessageBoard = {
        var element = document.getElementById("message");
        element.scrollTop = element.scrollHeight;
    
-       
+       //Här är funktionen för att visa tiden för meddelanden
        imgClock.onclick = function()
        {
            alert( MessageBoard.messages[arrayslot].getFormatedDateText() );
--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,14 +2,45 @@
 
 var memory = {
     
-    
+    memoryArray: [],
     
     init:function(){
         
+        var rows = 4;
+        var cols = 4;
+        
+        
+        memory.memoryArray = RandomGenerator.getPictureArray(rows, cols);
+        console.log(memory.memoryArray.length);
+        console.log(memory.memoryArray);
+       
+        //Skapa en tabell
+        memory.table = document.createElement("table");
+        memory.table.border = 2;
+    
+        //Skapa ett spelbord
+        memory.gameBoard = document.getElementById("memory_board");
+        
+        //Här skrivs den vågräta spelraden ut
+        for (var i = 0; i < memory.rows; i += 1)
+        {
+           
+            
+           //Här skrivs varje cell ut
+            for (var j = 0; j < memory.cols; j += 1)
+            {
+           
+               
+            }
+            
+            
+        }
         
         
         
-    }
+    },
+    
+    
     
     
     
@@ -26,3 +57,5 @@ var memory = {
     
     
 };
+
+window.onload = memory.init;
