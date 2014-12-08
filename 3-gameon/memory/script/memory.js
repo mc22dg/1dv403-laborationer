c0bf8ab6a630d7482e879121adee09df45d14e62
Martin Carlsson <mc22dg@student.lnu.se>
2014-12-08 14:31:14 +0000

    up klar

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,12 +4,15 @@ var memory = {
     
     memoryArray: [],
     counter: 0,
+    tryCount: 0,
     last: null ,
+    
+    pairs: 0, //Hur många par som finns
     init:function(){
         
         var rows = 4; //rader
         var cols = 4; //kolumner
-        
+        memory.pairs = rows * cols / 2;
         
         memory.memoryArray = RandomGenerator.getPictureArray(rows, cols);
         console.log(memory.memoryArray);
@@ -33,9 +36,12 @@ var memory = {
                 var link = document.createElement("a");
                 var img = document.createElement("img");
                 
+                //# = gör länken klickbar
                 link.href = "#";
                 link.onclick = memory.flipTile;
+                link.flipped = false;
                 
+                //"formel" för index (position) för ikonerna i tabellen
                 var n = j * cols + i;
                 
                 link.number = memory.memoryArray[n];
@@ -60,23 +66,58 @@ var memory = {
         }
         
         //append child
-        document.body.appendChild(memory.table);
+        document.getElementById("table").appendChild(memory.table);
     },
     
     
     flipTile:function(){
+        
+        if(this.flipped){
+            return;
+        }    
+        
+        this.flipped = true;
+         
         this.img.src = "pics/"+ this.number +".png";
         memory.counter++;
         
-        
+        //om senaste brickan inte är null
         if(memory.last != null){
-        
-            if(memory.last.number == this.number){
-                console.log("hurra")
+            
+            //om räknar är större eller lika med 2 (då har vi tagit två brickor)
+            if(memory.counter >= 2){
+                memory.tryCount++;
+                console.log(memory.tryCount);
+               
+                //om senaste och sista brickan (siffran) är samma 
+                if(memory.last.number == this.number){
+                    console.log("hurra")
+                    memory.pairs--;
+                    if(memory.pairs == 0){
+                        alert("Grattis!" + " Du klarade det på "+ memory.tryCount +" försök.");
+                    }
+                }
+                else{
+                    
+                    
+                    var last = memory.last;
+                    var that = this;
+                    
+                    window.setTimeout(function() {
+                        
+                        last.img.src = "pics/0.png";
+                        that.img.src = "pics/0.png";
+                        that.flipped = false;
+                        last.flipped = false;
+                    }, 1000);
+                    
+                }
+                
+                
+                memory.counter = 0
             }
-        
         }
-        console.log(this.number);
+        
         memory.last = this;
     }
     
