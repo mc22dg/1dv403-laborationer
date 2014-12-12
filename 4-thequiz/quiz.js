76e24cc186b8d1a67faf5290d627b22ec506fcce
Martin Carlsson <mc22dg@student.lnu.se>
2014-12-12 16:21:34 +0000

    nästan klar med js

--- 4-thequiz/quiz.js
+++ 4-thequiz/quiz.js
@@ -4,19 +4,21 @@ window.onload = function(){
     
     var quiz = new Quiz();
     
+    //Här är länken för frågorna
     quiz.fetchQuestion("http://vhost3.lnu.se:20080/question/1");
   
 };
 
+//Konstruktorfunktionen
 function Quiz(){
     
     this.talker = new XMLHttpRequest();
 }
 
 
-Quiz.prototype.process = function(rawdata){
+Quiz.prototype.process = function(rawdata,status){
     
-   //översätt data
+   //översätt datan
     var data = JSON.parse(rawdata);
     
     //presentera meddelandet (kalla på det)
@@ -24,11 +26,34 @@ Quiz.prototype.process = function(rawdata){
    
     //fått en fråga
     if (data.question) {
-        this.showQuestion(data.question)
+        this.showQuestion(data.question);
+        var button = document.getElementById("button");
+        var that = this;
+        button.onclick = function() {
+            that.sendAnswer(data.nextURL);
+        };
     }
-    //ja
-    
-    //nej
+    else {
+        if(status == 200){
+            if(data.nextURL){
+                this.fetchQuestion(data.nextURL);
+            }
+            else{
+                var done = document.getElementById("done");
+                done.innerHTML = "Du klarade alla frågor!";
+                this.showMessage("");
+                this.sendAnswer("");
+                this.showResult("");
+                
+            }
+            this.showResult(data.message);
+        }
+        else{
+            this.showResult("");
+            
+        }
+    }
+
     
     console.log(data);
 };
@@ -47,6 +72,11 @@ Quiz.prototype.showQuestion = function(text){
     
 };
 
+Quiz.prototype.showResult = function(text){
+    var result = document.getElementById("result");
+    result.innerHTML = text;
+};
+
 
 Quiz.prototype.fetchQuestion = function(url){
   var that = this;
@@ -63,8 +93,24 @@ Quiz.prototype.fetchQuestion = function(url){
     this.talker.send(null);
 };
 
-Quiz.prototype.sendAnswer = function(){
+Quiz.prototype.sendAnswer = function(url){
   
+  var input = document.getElementById("comment").value;
+  var json = JSON.stringify( { answer: input } );
+  console.log(json + " url: " + url);
   
+  var that = this;
+  
+   this.talker.onreadystatechange = function(){
+        
+        if (this.readyState === 4) {
+            that.process(this.responseText,this.status);
+        }
+        
+    };
+    
+    this.talker.open("POST", url, true);
+    this.talker.setRequestHeader("Content-Type","application/json");
+    this.talker.send(json);
   
 };
